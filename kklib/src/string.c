613b11cc616e928318aaf63ff5d335b4bd68ed35
Tim Whiting <tim@whitings.org>
2024-04-06 20:37:28 -0600

    fix vector<char>.string conversion

--- kklib/src/string.c
+++ kklib/src/string.c
@@ -701,7 +701,7 @@ kk_string_t kk_string_from_chars(kk_vector_t v, kk_context_t* ctx) {
     len += kk_utf8_len(kk_char_unbox(cs[i], KK_BORROWED, ctx));
   }
   uint8_t* p;
-  kk_string_t s = kk_unsafe_string_alloc_buf(len + 1, &p, ctx);
+  kk_string_t s = kk_unsafe_string_alloc_buf(len, &p, ctx); // allocation already handles the len+1 and terminating 0
   for (kk_ssize_t i = 0; i < n; i++) {
     kk_ssize_t count;
     kk_utf8_write(kk_char_unbox(cs[i], KK_BORROWED, ctx), p, &count);
